b7e5ecc95b6b24b00a6f4c4254f6b1a5653ac376
Joasia Klasik <asklas@op.pl>
2024-03-06 17:55:52 +0100

    problem class interface

--- Zadanie 1/Problem/inc/Problem.hpp
+++ Zadanie 1/Problem/inc/Problem.hpp
@@ -1,17 +1,39 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+#include "Task.hpp"
 
 class Problem
 {
 private:
-    /* data */
+    /* private data */
+    std::vector<Task> m_tasks;
+    int m_tasksNr;
 public:
-    Problem(/* args */);
+    /* public methods */
+    Problem(std::vector<Task> tasks, int tasksNr);
     ~Problem();
+
+    void GenerateInstance();
+    void ReadFromFile(std::ifstream file);
+    void AlgorithmSortRj();
+    void AlgorithmSortQj();
+    void AlgorithmCompleteReview();
+
 };
 
-Problem::Problem(/* args */)
+Problem::Problem(std::vector<Task> tasks, int tasksNr) : m_tasks(tasks)
 {
+    for(int i = 0; i < tasksNr; i++)
+    {
+        Task *newTask = new(Task);
+        m_tasks.push_back(*newTask);
+    }
 }
 
 Problem::~Problem()
 {
+    m_tasks.clear();
 }
