a07f5f55f5666b14e1568fd2510800bf1a3d429e
Joasia Klasik <asklas@op.pl>
2024-03-06 21:15:43 +0100

    task constructor

--- Zadanie 1/Task/inc/Task.hpp
+++ Zadanie 1/Task/inc/Task.hpp
@@ -9,9 +9,14 @@ private:
 
 public:
     /* public methods */
+    Task();
     Task(double pj, double rj, double qj);
     ~Task();
 };
+
+Task::Task() : m_pj(0), m_rj(0), m_qj(0)
+{
+}
 
 Task::Task(double pj, double rj, double qj) : m_pj(pj), m_rj(rj), m_qj(qj)
 {
