1130f31a07106a979ddd24615630347610db6a37
Joasia Klasik <asklas@op.pl>
2024-03-06 21:21:11 +0100

    move task operator

--- Zadanie 1/Task/inc/Task.hpp
+++ Zadanie 1/Task/inc/Task.hpp
@@ -12,16 +12,6 @@ public:
     Task();
     Task(double pj, double rj, double qj);
     ~Task();
+    Task & operator = (const Task * task);
 };
 
-Task::Task() : m_pj(0), m_rj(0), m_qj(0)
-{
-}
-
-Task::Task(double pj, double rj, double qj) : m_pj(pj), m_rj(rj), m_qj(qj)
-{
-}
-
-Task::~Task()
-{
-}
--- Zadanie 1/Task/src/Task.cpp
+++ Zadanie 1/Task/src/Task.cpp
@@ -1,1 +1,23 @@
 #include "Task.hpp"
+
+
+Task::Task() : m_pj(0), m_rj(0), m_qj(0)
+{
+}
+
+Task::Task(double pj, double rj, double qj) : m_pj(pj), m_rj(rj), m_qj(qj)
+{
+}
+
+Task::~Task()
+{
+}
+
+Task & Task::operator = (const Task & task)
+{
+    m_pj = task.m_pj;
+    m_rj = task.m_rj;
+    m_qj = task.m_qj;   
+
+    return *this;
+}
