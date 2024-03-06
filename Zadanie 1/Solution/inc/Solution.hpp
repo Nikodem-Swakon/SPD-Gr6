b1c8410ccf2441f0fa1ecbb7f612f400176001f7
Joasia Klasik <asklas@op.pl>
2024-03-06 15:24:01 +0100

    solutio class interface

--- Zadanie 1/Solution/inc/Solution.hpp
+++ Zadanie 1/Solution/inc/Solution.hpp
@@ -1,15 +1,33 @@
-
+#pragma once
+#include <iostream>
+#include <vector>
 class Solution
 {
 private:
-    /* data */
+    /* private data */
+    double m_criterion;
+    std::vector<double> m_ranked; 
 public:
-    Solution(/* args */);
+    /* public methods */
+    Solution(double m_criterion, std::vector<double> ranked);
+    Solution(Solution & solution);
     ~Solution();
 };
 
-Solution::Solution(/* args */)
+Solution::Solution(double criterion, std::vector<double> ranked) : m_criterion(criterion)
 {
+    for(int i = 0; i < ranked.size(); i++)
+    {
+        m_ranked.push_back(ranked[i]);
+    }
+}
+
+Solution::Solution(Solution & solution) : m_criterion(solution.m_criterion)
+{
+    for(int i = 0; i < solution.m_ranked.size(); i++)
+    {
+        m_ranked.push_back(solution.m_ranked[i]);
+    }
 }
 
 Solution::~Solution()
