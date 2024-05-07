4e1dc76204083b7ff00d016d500e3ca595c82731
BuildTools <unconfigured@null.spigotmc.org>
2024-05-07 10:07:00 +0200

    mierzenie czasu

--- Zadanie 2/main.cpp
+++ Zadanie 2/main.cpp
@@ -110,56 +110,91 @@ int main(int argc, char *argv[])
     std::vector<Task> taskVector;
     readFromFile(filename, taskVector);
     Problem problem(taskVector);
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
-    problem.LPT();
-    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-std::cout << "Time LPT: " << duration.count() << " microseconds" << std::endl;
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+std::cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  LSA  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
+
+    std::chrono::high_resolution_clock::time_point startLSA = std::chrono::high_resolution_clock::now();
     problem.LSA();
-    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-std::cout << "Time LSA: " << duration.count() << " microseconds" << std::endl;
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+    std::chrono::high_resolution_clock::time_point endLSA = std::chrono::high_resolution_clock::now();
+    auto durationLSA = std::chrono::duration_cast<std::chrono::microseconds>(endLSA - startLSA);
+std::cout << "Time LSA: " << durationLSA.count() << " microseconds" << std::endl;
+std::cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% LPT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
+
+    std::chrono::high_resolution_clock::time_point startLPT = std::chrono::high_resolution_clock::now();
+    problem.LPT();
+    std::chrono::high_resolution_clock::time_point endLPT = std::chrono::high_resolution_clock::now();
+    auto durationLPT = std::chrono::duration_cast<std::chrono::microseconds>(endLPT - startLPT);
+std::cout << "Time LPT: " << durationLPT.count() << " microseconds" << std::endl;
+std::cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%FPTAS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
+
+    std::chrono::high_resolution_clock::time_point startFPTAS = std::chrono::high_resolution_clock::now();
+    problem.FPTAS(2);
+    std::chrono::high_resolution_clock::time_point endFPTAS = std::chrono::high_resolution_clock::now();
+    auto durationFPTAS = std::chrono::duration_cast<std::chrono::microseconds>(endFPTAS - startFPTAS);
+std::cout << "Time FPTAS: " << durationFPTAS.count() << " microseconds" << std::endl;
+std::cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%DYNAMIC PROGRAMING 2%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
+
+    std::chrono::high_resolution_clock::time_point startDynamicProgramming2D = std::chrono::high_resolution_clock::now();
+    problem.DynamicProgramming2D();
+    std::chrono::high_resolution_clock::time_point endDynamicProgramming2D = std::chrono::high_resolution_clock::now();
+    auto durationDynamicProgramming2D = std::chrono::duration_cast<std::chrono::microseconds>(endDynamicProgramming2D - startDynamicProgramming2D);
+std::cout << "Time DP2: " << durationDynamicProgramming2D.count() << " microseconds" << std::endl;
+std::cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  DYNAMIC PROGRAMING 3%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
+
+std::chrono::high_resolution_clock::time_point startDynamicProgramming3D = std::chrono::high_resolution_clock::now();
+ problem.DynamicProgramming3D();
+ std::chrono::high_resolution_clock::time_point endDynamicProgramming3D = std::chrono::high_resolution_clock::now();
+    auto durationDynamicProgramming3D = std::chrono::duration_cast<std::chrono::microseconds>(endDynamicProgramming3D - startDynamicProgramming3D);
+std::cout << "Time DP3: " << durationDynamicProgramming3D.count() << " microseconds" << std::endl;
+std::cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% COMPLETE REVIEV 2%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
+
+    std::chrono::high_resolution_clock::time_point startCompleteReview_2 = std::chrono::high_resolution_clock::now();
      problem.CompleteReview_2();
-    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-std::cout << "Time CR2: " << duration.count() << " microseconds" << std::endl;
+    std::chrono::high_resolution_clock::time_point endCompleteReview_2 = std::chrono::high_resolution_clock::now();
+    auto durationCompleteReview_2 = std::chrono::duration_cast<std::chrono::microseconds>(endCompleteReview_2 - startCompleteReview_2);
+std::cout << "Time CR2: " << durationCompleteReview_2.count() << " microseconds" << std::endl;
+std::cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   COMPLETE REVIEV 3%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
 
-
-
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+    std::chrono::high_resolution_clock::time_point startCompleteReview_3 = std::chrono::high_resolution_clock::now();
     problem.CompleteReview_3();
-    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-std::cout << "Time CR3: " << duration.count() << " microseconds" << std::endl;
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+    std::chrono::high_resolution_clock::time_point endCompleteReview_3 = std::chrono::high_resolution_clock::now();
+    auto durationCompleteReview_3 = std::chrono::duration_cast<std::chrono::microseconds>(endCompleteReview_3 - startCompleteReview_3);
+std::cout << "Time CR3: " << durationCompleteReview_3.count() << " microseconds" << std::endl;
+std::cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  PTAS 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
+
+
+
+
+    std::chrono::high_resolution_clock::time_point startPTAS_2 = std::chrono::high_resolution_clock::now();
+    problem.PTAS_2(2);
+    std::chrono::high_resolution_clock::time_point endPTAS_2 = std::chrono::high_resolution_clock::now();
+    auto durationPTAS_2 = std::chrono::duration_cast<std::chrono::microseconds>(endPTAS_2 - startPTAS_2);
+std::cout << "Time PTAS2: " << durationPTAS_2.count() << " microseconds" << std::endl;
+std::cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  PTAS 3 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
+
+
+
+
+    std::chrono::high_resolution_clock::time_point startPTAS_3 = std::chrono::high_resolution_clock::now();
      problem.PTAS_3(2);
-    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-std::cout << "Time PTAS3: " << duration.count() << " microseconds" << std::endl;
+    std::chrono::high_resolution_clock::time_point endPTAS_3 = std::chrono::high_resolution_clock::now();
+    auto durationPTAS_3 = std::chrono::duration_cast<std::chrono::microseconds>(endPTAS_3 - startPTAS_3);
+std::cout << "Time PTAS3: " << durationPTAS_3.count() << " microseconds" << std::endl;
+std::cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<std::endl;
+
+
+
+
+
+
     //std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
     //problem.DisplayTasks();
     //std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
     //auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 //std::cout << "Time LPT: " << duration.count() << " microseconds" << std::endl;
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
-    problem.DynamicProgramming2D();
-    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-std::cout << "Time DP2: " << duration.count() << " microseconds" << std::endl;
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
-    problem.FPTAS(2);
-    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-std::cout << "Time FPTAS2: " << duration.count() << " microseconds" << std::endl;
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
-    problem.PTAS_2(2);
-    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-std::cout << "Time PTAS2: " << duration.count() << " microseconds" << std::endl;
 
- problem.DynamicProgramming3D();
+
+
+
 
     return 0;
 }
